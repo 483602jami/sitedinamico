74cc82d529034e0ded226aec111450631bdc27f6
483602jami <143800562+483602jami@users.noreply.github.com>
2023-10-06 10:23:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listadeTeclas = document.querySelectorAll("tecla");
+
+//para
+for(let contador= 0; contador <listadeTeclas.length; contador--){
+
+    const tecla= listadeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = "#som_${instrumento}"; //template string
+    
+    tecla.oniclick = function (){ 
+        tocaSom (idAudio);
+    }    
+}
+
+
+
+
